f9fc175295a0d9a3e1f74dd9365dfc31b75b0047
safouene <benalaya.safouene.com>
2017-06-04 00:46:32 +0100

    add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,49 @@
+const MongoClient = require('mongodb').MongoClient;
+//the code above is the same with object distructuring 
+//const {MongoClient} = require('mongodb');
+
+
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+
+	if (err){
+		return console.log('unable to connect to mongodb srever');
+	}
+
+console.log('connected to mangodb server');
+
+	
+	db.collection('Todos').find({text : 'sone text from mongorobo'}).toArray()
+	.then((docs) => {
+		console.log('Todos -----');
+			console.log(JSON.stringify(docs, 'undefind', 2));
+
+
+	}, (err) =>{
+		console.log('unable fo fetch Todos',err);
+	});
+
+	//delete many
+	// db.collection('Todos').deleteMany({text : 'an other to do'})
+	// .then((result) => {
+	// 		console.log(result);
+	// });
+
+
+	//delete one
+	db.collection('Todos').deleteOne({text : 'something to do'})
+	.then((result) => {
+			console.log(result);
+	});
+
+	//find one and delete 
+
+	//db.close 
+
+		
+
+	
+
+	//db.close();
+});
