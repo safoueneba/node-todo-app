0b849a003a5975642ac0f41baaf6e5080def57d7
safouene <benalaya.safouene.com>
2017-06-04 01:32:24 +0100

    add update script

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -1,4 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+
 //the code above is the same with object distructuring 
 //const {MongoClient} = require('mongodb');
 
@@ -14,18 +16,26 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 console.log('connected to mangodb server');
 
 	
-	db.collection('Todos').find({text : 'sone text from mongorobo'}).toArray()
-	.then((docs) => {
-		console.log('Todos -----');
-			console.log(JSON.stringify(docs, 'undefind', 2));
+	// db.collection('Todos').find({text : 'sone text from mongorobo'}).toArray()
+	// .then((docs) => {
+	// 	console.log('Todos -----');
+	// 		console.log(JSON.stringify(docs, 'undefind', 2));
 
 
-	}, (err) =>{
-		console.log('unable fo fetch Todos',err);
-	});
+	// }, (err) =>{
+	// 	console.log('unable fo fetch Todos',err);
+	// });
 
 	
 // updating script 
+
+	db.collection('Users').findOneAndUpdate(
+		{_id : new ObjectID('59332d16ed3da20e2cb07772')},
+	 { $set : {namr : 'safouene ba updated name'}, $inc: { age : +2}}, 
+	 { returnOriginal : false})
+	.then((result) => {
+		console.log(result);
+	});
   
-  
+  db.close();
 });
